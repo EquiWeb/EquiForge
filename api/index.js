e7dbb918408c29ef436849a1ab41f6cefd9fbbd2
Ciaran McAleer <72049678+CiaranMcAleer@users.noreply.github.com>
2026-03-02 16:14:59 +0000

    Use srvx node handler for Start SSR

--- api/index.js
+++ api/index.js
@@ -1,21 +1,4 @@
 import server from './server.js'
+import { toNodeHandler } from 'srvx/node'
 
-export default async function handler(req, res) {
-  try {
-    const response = await server.fetch(req)
-    res.statusCode = response.status
-    response.headers.forEach((value, key) => {
-      res.setHeader(key, value)
-    })
-    const body = Buffer.from(await response.arrayBuffer())
-    res.end(body)
-  } catch (error) {
-    res.statusCode = 500
-    res.setHeader('Content-Type', 'application/json')
-    res.end(
-      JSON.stringify({
-        error: error instanceof Error ? error.message : 'Server error',
-      }),
-    )
-  }
-}
+export default toNodeHandler(server.fetch)
